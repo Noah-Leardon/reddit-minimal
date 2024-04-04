85e3a40ccd1df297137c5158d397695d94cd51ef
Noah Leardon <noahleardon@Noahs-MBP.home>
2024-04-04 14:31:31 -0400

    deleted unused slice and updated store

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import commentsReducer from "../Slices/CommentsSlice/commentsSlice"
 import postReducer from "../Slices/PostSlice/postSlice"
-import callCountReducer from "../Slices/CallCountSlice/callCountSlice"
 
 const store = configureStore({
     reducer: {
         comments: commentsReducer,
         post: postReducer,
-        callCount: callCountReducer
     }
 })
 
